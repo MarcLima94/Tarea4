308f0a47b18974578f25b876b37b49c53adf1296
Marc Lima <marc.calzifer@gmail.com>
2018-01-19 19:14:34 -0600

    first commit

---
+++ JS/Tarea4.js
@@ -0,0 +1,29 @@
+;
+
+((d,c) => {
+
+    /**Declarar variables**/
+const pWhatIsEvent = d.getElementById('pelis-y-más'),
+      btnHello = d.getElementById('say-hi'),
+      btnHeaderColor = d.getElementById('cambio-de-color'),
+      cards = d.querySelectorAll('cambio-de-tarjeta'),
+      clock = d.querySelector('.clock'),
+      clockTitle = d.querySelector('.clock-title'),
+      startClock = d.getElementById('start-clock'),
+      stopClock = d.getElementById('parar-clock'),
+      startAlarm = d.getElementById('iniciar-alarma'),
+      stopAlarm = d.getElementById('detener-alarma'),
+      alarm = d.createElement('reloj')
+
+      
+      
+
+
+    /**Declaración de funciones**/
+    alert ('Checa bien') 
+
+
+    /**Ejecución de código**/
+
+
+})(document,console.log)
