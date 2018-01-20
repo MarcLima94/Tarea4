bb6d11d30784d0b439e36db5763cab4a5f43f4ef
Marc Lima <marc.calzifer@gmail.com>
2018-01-19 21:53:33 -0600

    Update code. - Fix errors on Tarea4.js - Fix Css files with same line on id image.

--- JS/Tarea4.js
+++ JS/Tarea4.js
@@ -1,29 +1,56 @@
-;
+/**Declarar variables**/
+const c = console.log();
+const pWhatIsEvent = document.getElementById('pelis-y-más'),
+  btnHello = document.getElementById('say-hi'),
+  btnHeaderColor = document.getElementById('cambio-de-color'),
+  cards = document.querySelectorAll('cambio-de-tarjeta'),
+  clock = document.querySelector('.Clock'),
+  clockTitle = document.querySelector('.Clock-title'),
+  startAlarm = document.getElementById('start-alarm'),
+  stopAlarm = document.getElementById('stop-alarm'),
+  alarm = document.createElement('reloj'),
+  idHeader = document.getElementById("header");
 
-((d,c) => {
+let clockTempo,
+  alarmTempo;
 
-    /**Declarar variables**/
-const pWhatIsEvent = d.getElementById('pelis-y-más'),
-      btnHello = d.getElementById('say-hi'),
-      btnHeaderColor = d.getElementById('cambio-de-color'),
-      cards = d.querySelectorAll('cambio-de-tarjeta'),
-      clock = d.querySelector('.clock'),
-      clockTitle = d.querySelector('.clock-title'),
-      startClock = d.getElementById('start-clock'),
-      stopClock = d.getElementById('parar-clock'),
-      startAlarm = d.getElementById('iniciar-alarma'),
-      stopAlarm = d.getElementById('detener-alarma'),
-      alarm = d.createElement('reloj')
-
-      
-      
+// Button por change header section.
+btnHeaderColor.addEventListener('click', function () {
+  /** Color - Reference material colors from | https://www.materialui.co/colors
+   * http://www.color-hex.com/color/aed581 - Green color
+   * http://www.color-hex.com/color/9e9e9e - Grey color
+   */
+  if (idHeader.style.backgroundColor === 'rgb(158, 158, 158)') {
+    idHeader.style.backgroundColor = "#AED581";
+  } else {
+    idHeader.style.backgroundColor = "#9E9E9E";
+  }
+});
 
 
-    /**Declaración de funciones**/
-    alert ('Checa bien') 
 
+/*
+    = Asignación de valor
+    == Comparación de valores
+    === Comparación de valores y tipo de dato
+  */
 
-    /**Ejecución de código**/
+alarm.src = './sound/Nihilore'
+//alarm.controls = true
+//d.body.appendChild(alarm)
 
+/*
+//Sin Arrow Function
+startClock.addEventListener('click', function () {
+  clockTempo = setInterval(function () {
+    clock.textContent = new Date().toLocaleString()
+  }, 1000)
+})
+*/
+startAlarm.addEventListener('click', () => {
+  alert('Hola marc click en btn');
+  //alarmTempo = setTimeout(() => alarm.play(), 3000)
+  alarm.play()
+});
 
-})(document,console.log)
+stopAlarm.addEventListener('click', () => clearTimeout(alarmTempo))
